2cfe6d2df987bc2c93834391893d7cc669a32a92
valeriemay00 <vchavez8500@gmail.com>
2025-10-01 17:06:05 -0700

    Changed the TSconfig.json file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,18 @@
-import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
-import { SplashScreen } from "expo-router";
-import { PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
     Poppins: require("../assets/fonts/Poppins-Regular.ttf"),
   });
 
-  // Keep splash until fonts load
   useEffect(() => {
     if (fontsLoaded) SplashScreen.hideAsync();
   }, [fontsLoaded]);
 
   if (!fontsLoaded) return null;
 
-  // Wrap your Stack (navigator) in PaperProvider here
-  return (
-    <PaperProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </PaperProvider>
-  );
+  // Stack here just manages routing
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
